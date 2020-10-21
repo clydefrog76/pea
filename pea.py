771047a67b0a8fee1c9710048a2312dd021a4962
clydefrog76 <alexander.teusch@gmail.com>
2020-10-21 11:35:41 +0200

    code update

--- pea.py
+++ pea.py
@@ -15,10 +15,9 @@
         https://github.com/clydefrog76/pea
 """
 
-import os, socket, sys, json, time, ast, datetime, binascii, asyncio, platform, shlex
+import os, socket, sys, json, time, ast, datetime, binascii, asyncio, platform, shlex, importlib
 import tkinter.ttk as ttk
 from tkinter import Tk, filedialog, messagebox, VERTICAL, TRUE, FALSE, Text, Listbox, Canvas, Frame, Menu, PhotoImage, NW, YES, NO, BOTH, LEFT, RIGHT, END, TOP, BOTTOM, Y, X, Toplevel, IntVar, StringVar, TclError
-import importlib
 
 async def run_tk(root, interval=0.01):
     """
